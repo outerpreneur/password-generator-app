25a3313bc0ce19484b977b2e4ed8150b0bf5061a
Aleksander Tysklind <alek@outerpreneur.com>
2023-11-20 22:46:52 +0100

    changes

--- app.js
+++ app.js
@@ -40,42 +40,70 @@ generateButton.addEventListener("click", (event) => {
     }
   });
 
-  if (checkedCount === 1) {
-    document.getElementById("too-weak").classList.add("bg-red-300");
-    document.getElementById("weak").classList.remove("bg-yellow-300");
-    document.getElementById("medium").classList.remove("bg-yellow-300");
-    document.getElementById("strong").classList.remove("bg-yellow-300");
+  if (checkedCount <= 1) {
+    document
+      .getElementById("strength-bars")
+      .querySelectorAll("div")
+      .forEach((block) => {
+        block.classList.remove("bg-green-300");
+        block.classList.remove("bg-orange-300");
+        block.classList.remove("bg-yellow-300");
+      });
+    document.getElementById("too-weak").classList.add("bg-red-500");
     document.getElementById("strenth-level").innerText = "TOO WEAK!";
   }
 
   if (checkedCount === 2) {
+    document
+      .getElementById("strength-bars")
+      .querySelectorAll("div")
+      .forEach((block) => {
+        block.classList.remove("bg-green-300");
+        block.classList.remove("bg-red-300");
+        block.classList.remove("bg-yellow-300");
+      });
     document.getElementById("too-weak").classList.add("bg-orange-300");
     document.getElementById("weak").classList.add("bg-orange-300");
-    document.getElementById("medium").classList.remove("bg-yellow-300");
-    document.getElementById("strong").classList.remove("bg-yellow-300");
     document.getElementById("strenth-level").innerText = "WEAK";
   }
 
   if (checkedCount === 3) {
+    document
+      .getElementById("strength-bars")
+      .querySelectorAll("div")
+      .forEach((block) => {
+        block.classList.remove("bg-green-300");
+        block.classList.remove("bg-red-300");
+        block.classList.remove("bg-orange-300");
+      });
+
     document.getElementById("too-weak").classList.add("bg-yellow-300");
     document.getElementById("weak").classList.add("bg-yellow-300");
-    document.getElementById("weak").classList.remove("bg-red-300");
     document.getElementById("medium").classList.add("bg-yellow-300");
-    document.getElementById("strong").classList.remove("bg-yellow-300");
     document.getElementById("strenth-level").innerText = "MEDIUM";
   }
 
   if (checkedCount === 4) {
+    document
+      .getElementById("strength-bars")
+      .querySelectorAll("div")
+      .forEach((block) => {
+        block.classList.remove("bg-yellow-500");
+        block.classList.remove("bg-red-300");
+        block.classList.remove("bg-orange-300");
+      });
     document.getElementById("too-weak").classList.add("bg-green-300");
     document.getElementById("weak").classList.add("bg-green-300");
     document.getElementById("medium").classList.add("bg-green-300");
     document.getElementById("strong").classList.add("bg-green-300");
     document.getElementById("strenth-level").innerText = "STRONG";
   }
+  console.log(checkedCount);
 });
 
 clipBoard.addEventListener("click", () => {
   navigator.clipboard.writeText(password.innerText);
+  document.getElementById("copy-text").classList.remove("hidden");
 });
 
 // Functions
@@ -95,6 +123,18 @@ function getRandomNumber() {
 function getRandomSymbol() {
   var symbols = "!@#$%^&*(){}[]=<>/,.";
   return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function updateSliderColor(slider) {
+  // Get the current value of the slider
+  const sliderValue = slider.value;
+
+  // Calculate a percentage based on the slider's position
+  const percentage =
+    ((sliderValue - slider.min) / (slider.max - slider.min)) * 100;
+
+  // Update the gradient color dynamically
+  slider.style.background = `linear-gradient(to right, #A4FFAF ${percentage}%, #fff ${percentage}%)`;
 }
 
 //
